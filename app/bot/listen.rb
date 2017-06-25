c19dd100211c41d40234c21070358261210a5292
longnh <long@hipvan.com>
2017-06-25 12:49:30 +0700

    Init Messenger Bot app

---
+++ app/bot/listen.rb
@@ -0,0 +1,12 @@
+require "facebook/messenger"
+include Facebook::Messenger
+Facebook::Messenger::Subscriptions.subscribe(access_token: ENV["ACCESS_TOKEN"])
+
+Bot.on :message do |message|
+  Bot.deliver({
+    recipient: message.sender,
+    message: {
+      text: message.text
+    }
+  }, access_token: ENV["ACCESS_TOKEN"])
+end
